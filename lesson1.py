bb3bd397ae47a85014810d03945d8f25ec7c33bf
khanbaba-ahamaliiev <khanbaba.ahamaliiev1@gmail.com>
2026-06-23 20:56:05 +0400

    lesson 1

--- lesson1.py
+++ lesson1.py
@@ -1,110 +1,85 @@
 import numpy as np
 
-# # створення масиву
-# array = np.array([1, 2, 3, 4, 5])
-#
-# print(array)
-# print(array.shape)  # розмір масиву
-# print(array.dtype)  # тип даних в одній комірці масиву
-#
-# # двовимірний масив(таблиця/матриця)
-# array = np.array([[1, 2, 3, 4, 5],
-#                   [6, 7, 8.5, 9, 10]])
-#
-# print(array)
-# print(array.shape)  # розмір масиву
-# print(array.dtype)  # тип даних в одній комірці масиву
+nums = np.array([1, 2, 3, 4]) # массив
+print(nums)
+print(type(nums))
+
+print(nums.shape)
+print(nums.dtype)
 
 
-# # порівнння швидкості для масивів та списків
-# import time
-#
-#
-# N = 10_000_000
-# nums_list = list(range(N))
-# nums_array = np.array(range(N))
-#
-# start = time.time()
-# res = sum(nums_list)
-# end = time.time()
-#
-# print(f"Python list: {end - start:0.5f} sec")
-#
-# start = time.time()
-# res = np.sum(nums_array)
-# end = time.time()
-#
-# print(f"Numpy array: {end - start:0.5f} sec")
+# двумерный массив
+nums = np.array(
+    [[1, 2, 3, 4],
+    [5, 6, 7, 8],
+     [9, 10, 11, 12]])
 
-
-# завжди використовувати функції numpy
-# цикл for -- це велике зло
-
-# # створення та розміри
-#
-# nums = np.arange(10, 20, 2)  # масив з діапазону, як в range
-# print(nums)
-#
-# nums = np.zeros(shape=(3, 4))  # масив з нулів
-# print(nums)
-#
-# # змінити розмір масиву
-# nums = np.arange(10, 20)
-# new_nums = nums.reshape((2, 5))
-# print(new_nums)
-# print(new_nums.shape)
-
-# індексація
-# nums = np.arange(10, 20)
-#
-# print(nums)
-# # print(nums[2])
-# # print(nums[2:5])  # зріз з 2(включно) по 5(не включно)
-# # print(nums[2:7:2])  # зріз з 2(включно) по 7(не включно) крок 2
-# # print(nums[:3])    # перші 3
-# # print(nums[-3:])  # останні 3
-#
-# nums[2] = 0
-#
-# #nums[2:7] = 0
-# nums[2:7] *= -1
-#
-# print(nums)
-
-# nums = np.array([[1, 2, 3, 4, 5],
-#                   [6, 7, 8.5, 9, 10]])
-#
-# print(nums.shape)  # рядки, стовпчики
-# # для двовимірних масивів йже 2 індекса -- рядка та стовпчика
-#
-# print(nums[1, 2])    # 8.5
-# print(nums[1])       # рядок з індексом 1
-# print(nums[1, 2:4])  # рядок з індексом 1, стовпчики з 2 по 4
-# print(nums[0:2, 1:4])  # блок рядки з 0 по 2 та стовпчики з 1 по 4
-# print(nums[:, 3])    # стовпчик 3
-#
-# nums[0:2, 1:4] += 100
-# print(nums)
-
-# булеві маски
-nums = np.array([[1, 20, 3, 4, 5],
-                   [6, 7, 8.5, 9, 10]])
-
-mask = nums > 7
-
-print(mask)
-print(mask.dtype)
-
-print(nums[mask])  # дістати числа які відповідають масці(умові)
-
-nums[mask] = 0
 print(nums)
 
-# дістати числа які не відповідають масці(умові)
-# and -- &
-# or  -- |
-# not -- ~
-print(nums[~mask])
+# Создание массивов
+# со списка
+nums_list = [1, 2, 3, 4]
+nums = np.array(nums_list)
 
-# кількість чисел що білше 7
-print(np.sum(mask))
+# аналог range
+nums = np.arange(10,20)
+print(nums)
+
+# массив нулей, 1 или случайных
+nums = np.zeros((6,))
+print(nums)
+
+nums = np.ones((3,3))
+print(nums)
+
+nums = np.random.rand(2, 3)
+print(nums)
+
+# изменение размеров и типов
+
+nums = np.arange(12)
+print(nums)
+print(nums.shape)
+print(nums.dtype)
+
+new_nums = nums.reshape(3,4)
+print(new_nums)
+print(new_nums.shape)
+print(new_nums.dtype)
+
+nums_float16 = nums.astype(np.float16)
+print(nums_float16)
+print(nums_float16.shape)
+print(nums_float16.dtype)
+
+
+# перенаполнение
+nums = np.array([10, 20, 30], dtype=np.int8) # int8 - это цифры от -128 до 127
+print(nums)
+
+
+# индексация
+nums = np.array([10, 20, 30, 40, 50])
+print(nums[0]) # - 1 элемент
+print(nums[-1]) # - последний элемент
+print(nums[1:3]) # - 20 - 40
+
+
+# индексация таблиц
+nums = np.arange(12).reshape(4,3)
+print(nums)
+
+# сперва рядки потом столбцы
+print(nums[1,2])
+print(nums[3]) # - весь ряд
+print(nums[0:2]) # - 2 первых ряда
+
+
+# булевые маски
+nums = np.array([15, 8, 12, 4, 1, 2])
+mask = nums > 10
+print(mask)
+
+# все элементы что отвечают маске
+print(nums[mask])
+
